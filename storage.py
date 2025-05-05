7abd25e6f14c112ef1f380cb1ad08883fd06788b
shmaks10 <mshepel@putnamscience.org>
2025-05-05 09:19:57 -0400

    05/05/2025

--- storage.py
+++ storage.py
@@ -6,15 +6,15 @@ class books():
         self.Books = {}
 
     def add(self):
-        self.title = input("Enter the name of the book you would like to add:\n")
-        self.author = input ("Enter the Author of the book you have just added:\n")
+        self.title = input("Enter the name of the book you would like to add:\n").upper()
+        self.author = input ("Enter the Author of the book you have just added:\n").upper()
         self.price = float(input("Enter the lsting price of the book:\n"))
         self.Books[self.title] = {'Author': self.author, 'Price':self.price}
         print(self.Books[self.title])
     
     def sell(self):
         while (True):
-            self.title = input("What book do you want to sell?\n")
+            self.title = input("What book do you like to sell a book?\n")
             if self.title in self.Books:
                 del self.Books[self.title]    
                 break
@@ -22,6 +22,10 @@ class books():
                 print ("Your book wan't found. Try again")
     def showbook(self):
         for x in self.Books:
-            print(x)
-            print(self.Books[x]['Author'])
-            print(self.Books[x]['Price'])
+            print("\n",x)
+            print("\n",self.Books[x]['Author'])
+            print("\n",self.Books[x]['Price'])
+    def savebook(self):
+        name = input("Enter the name of the file")
+        file = open(name, "x")
+        file.write
